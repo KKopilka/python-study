8e3d55e4429a663e6d3c009ce6b76884f66976d2
Yana <yana.stepanckova@yandex.ru>
2022-03-22 21:22:08 +0300

    cor

---
+++ ejfg.py
@@ -0,0 +1,12 @@
+import random
+from random import randint
+from random import randrange
+
+print(random.random())
+
+print(randint(-10,10))
+
+print(randrange(0, 10, 2))
+
+string = ['cat', 'dog', 'negr', 'tiger']
+print(random.choice(string))
